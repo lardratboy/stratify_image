9e9c2218f21ac888dac528e7c614aca8316ae01b
Brad P. Taylor <lardratboy@users.noreply.github.com>
2022-10-13 21:23:40 -0700

    quick implmentation

--- stratify_image.py
+++ stratify_image.py
@@ -1,5 +1,15 @@
 import PIL.Image
 
 def stratify_image(image):
-  pass    
-    
+  
+  w,h = image.size
+  bands = image.split()
+  o = PIL.Image.new(mode='L',size=(w*len(bands),h))
+  x = 0
+
+  for b in bands:
+    o.paste(b,(x,0))
+    x += w
+
+  return o
+  
